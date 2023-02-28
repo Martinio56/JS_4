866c0e4a4f922c6994b5ca75a48fa349a3b061e5
Nio <72229694+Martinio56@users.noreply.github.com>
2023-02-28 10:39:37 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+//Oppgave 1---------------------------------------------------------------------------------------------------------------------------
+
+// let tall = Math.round(Math.random()*100)
+// console.log(tall);
+// let svar = window.prompt('Skriv et tall')
+
+// while (svar) {
+//     if (svar < tall) {
+//         svar = window.prompt('Skriv et høyere tall')
+//     } else if (svar > tall) {
+//         svar = window.prompt('Skriv et mindre tall')
+//     } else {
+//         alert('Gratulerer!! Du har riktig tall')
+//     }
+// }
+
+//Oppgave 2---------------------------------------------------------------------------------------------------------------------------
+
+// for (let i = 0; i < 10; i++) {
+//     let rad = "";
+//     for (let j = 0; j < 10; j++) {
+//       rad += j + " ";
+//     }
+//     console.log(rad);
+//     document.write(rad+"<br>")
+//   }  
+
+//------------------------------------------------------------------------------------------------------------------------------------
